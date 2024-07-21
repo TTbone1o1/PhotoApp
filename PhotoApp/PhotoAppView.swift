206fce2995ae2b000e8f610e5fb6cacb860753ce
Abraham May <Abrahammay1015@gmail.com>
2024-07-21 15:50:52 -0500

    Just testing out git on mac terminal

---
+++ PhotoApp/PhotoAppView.swift
@@ -0,0 +1,24 @@
+//
+//  PhotoAppView.swift
+//  PhotoApp
+//
+//  Created by Abraham May on 7/21/24.
+//
+
+import SwiftUI
+
+struct PhotoAppView: View {
+    var body: some View {
+        Rectangle()
+            .fill(Color.blue)
+            .frame(width: 200, height: 200)
+            .cornerRadius(20)
+        Image(systemName: "star.fill")
+            .foregroundColor(.yellow)
+            
+    }
+}
+
+#Preview {
+    PhotoAppView()
+}
