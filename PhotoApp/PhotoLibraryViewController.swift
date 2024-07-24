50e217aff4e5ed9aed3ba4fe07c9d6ce1aa91c49
Abraham May <Abrahammay1015@gmail.com>
2024-07-24 11:52:51 -0500

    New file meant to store in photos taken by user

--- PhotoApp/PhotoLibraryViewController.swift
+++ PhotoApp/PhotoLibraryViewController.swift
@@ -1,18 +1,72 @@
-//
-//  PhotoLibraryViewController.swift
-//  PhotoApp
-//
-//  Created by Abraham May on 7/24/24.
-//
+import UIKit
+import Photos
 
-import SwiftUI
-
-struct PhotoLibraryViewController: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+class PhotoLibraryViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
+    
+    private var photos: [UIImage] = []
+    private let collectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .vertical
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.backgroundColor = .black
+        return collectionView
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.addSubview(collectionView)
+        collectionView.frame = view.bounds
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "cell")
+        
+        loadPhotos()
     }
-}
-
-#Preview {
-    PhotoLibraryViewController()
+    
+    private func loadPhotos() {
+        PHPhotoLibrary.requestAuthorization { status in
+            if status == .authorized {
+                let fetchOptions = PHFetchOptions()
+                fetchOptions.sortDescriptors = [NSSortDescriptor(key: "creationDate", ascending: false)]
+                let assets = PHAsset.fetchAssets(with: .image, options: fetchOptions)
+                
+                assets.enumerateObjects { (asset, _, _) in
+                    let imageManager = PHImageManager.default()
+                    let imageSize = CGSize(width: 300, height: 300)
+                    imageManager.requestImage(for: asset, targetSize: imageSize, contentMode: .aspectFill, options: nil) { image, _ in
+                        if let image = image {
+                            self.photos.append(image)
+                            DispatchQueue.main.async {
+                                self.collectionView.reloadData()
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+    
+    // MARK: - UICollectionViewDataSource
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return photos.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
+        let imageView = UIImageView(frame: cell.contentView.bounds)
+        imageView.image = photos[indexPath.item]
+        imageView.contentMode = .scaleAspectFill
+        imageView.clipsToBounds = true
+        cell.contentView.addSubview(imageView)
+        return cell
+    }
+    
+    // MARK: - UICollectionViewDelegateFlowLayout
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let width = (view.bounds.width - 20) / 3
+        return CGSize(width: width, height: width)
+    }
 }
